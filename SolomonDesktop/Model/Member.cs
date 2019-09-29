b59db0e3638a40255d9b08605faaabd9bcd95245
DeverexRU <DeverexRU@users.noreply.github.com>
2019-09-30 00:30:14 +0300

    Add files via upload

---
+++ SolomonDesktop/Model/Member.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolomonDesktop.Model
+{
+    class Member
+    {
+        string FullName { get; set; }
+        string Email { get; set; }
+    }
+}
---
+++ SolomonDesktop/Model/SoloEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolomonDesktop.Model
+{
+    class SoloEntity
+    {
+        string Caption { get; set; }
+
+        List<SoloEntity> DependsOnEntities { get; set; }
+
+
+
+          
+    }
+}
